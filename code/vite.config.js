f3ea8f6539f663ff14d29561499164c191fd51c8
Tom Des Heath <24888923@sun.ac.za>
2025-10-22 19:25:53 +0200

    Fixed vercel config

--- code/vite.config.js
+++ code/vite.config.js
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const isProduction = process.env.NODE_ENV === 'production'
+const isVercel = Boolean(process.env.VERCEL)
 const productionBasePath = '/designontarget/'
 
 export default defineConfig({
   plugins: [react()],
-  // Use the repository path when deploying to GitHub Pages
-  base: isProduction ? productionBasePath : '/',
+  // Use the repository path when deploying to GitHub Pages (non-Vercel)
+  base: isProduction && !isVercel ? productionBasePath : '/',
 })
